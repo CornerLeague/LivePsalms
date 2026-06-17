298b1cbfdc761e4e7fccc6c72b0ee25a4ede4165
cornerleague <support@cornerleague.com>
2026-06-17 16:26:08 -0700

    feat(study): StudyReader pane (wraps BibleReader in the indigo skin)

---
+++ src/notepad/study/panes/StudyReader.tsx
@@ -0,0 +1,18 @@
+// src/notepad/study/panes/StudyReader.tsx
+import { BibleReader } from '@/notepad/bible/BibleReader';
+
+export interface StudyReaderProps {
+  book: string;
+  chapter: number;
+  onPassageChange: (ref: { book: string; chapter: number }) => void;
+}
+
+export function StudyReader({ book, chapter, onPassageChange }: StudyReaderProps) {
+  return (
+    <BibleReader
+      initialBook={book}
+      initialChapter={chapter}
+      onPassageChange={(ref) => onPassageChange({ book: ref.book, chapter: ref.chapter })}
+    />
+  );
+}
